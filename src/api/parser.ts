8d344d5db0e2c703e49df6125e14795d961d4757
Thomas Couto (LAPTOP) <thomascouto@gmail.com>
2022-08-09 07:57:47 -0300

    feat: xml parser

---
+++ src/api/parser.ts
@@ -0,0 +1,38 @@
+import { X2jOptions, XMLParser } from 'fast-xml-parser'
+
+const alwaysArrayMetar = ['response.data.METAR']
+const alwaysArrayTaf = ['response.data.TAF']
+const alwaysArrayRotaer = [
+	'aisweb.lights.light',
+	'aisweb.runways.runway',
+	'aisweb.services.service',
+	'aisweb.rmkDistDeclared.rmkDist',
+]
+
+const generateOptions = (arr: string[]): X2jOptions =>
+	({
+		ignoreDeclaration: true,
+		ignoreAttributes: true,
+		attributeNamePrefix: '@_',
+		isArray: (name, jpath, isLeafNode, isAttribute) => {
+			if (arr.indexOf(jpath) !== -1) return true
+		},
+	} as X2jOptions)
+
+export const parseRequest = {
+	rotaer: (icao: string): RotaerResponse | RotaerResponseError => {
+		return new XMLParser(generateOptions(alwaysArrayRotaer)).parse(icao)
+	},
+
+	metar: (icao: string): MetarResponse => {
+		return new XMLParser(generateOptions(alwaysArrayMetar)).parse(icao)
+	},
+
+	taf: (icao: string): TafResponse => {
+		return new XMLParser(generateOptions(alwaysArrayTaf)).parse(icao)
+	},
+
+	srss: (icao: string): SRSSResponse => {
+		return new XMLParser().parse(icao)
+	},
+}
